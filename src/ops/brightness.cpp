7344d64c2754856a68cd78ed4dadd5e28743d683
Yiğit Dağıdır <yigitdgdr13@gmail.com>
2026-06-05 12:27:28 +0200

    add operation interface

---
+++ src/ops/brightness.cpp
@@ -0,0 +1,12 @@
+void BrightnessOp::setupTrackbars(const std::string& win) {
+    cv::createTrackbar("Brightness", win, &brightness_, 200, appTrackbarCb);
+    cv::createTrackbar("Contrast", win, &contrast_, 200, appTrackbarCb);
+}
+
+cv::Mat BrightnessOp::apply(const cv::Mat& src) const {
+    const double alpha = contrast_ / 100.0 ;
+    const int    beta = brightness_ - 100 ;   
+    cv::Mat out;
+    src.convertTo(out, -1, alpha, beta);
+    return out;
+}
